cff09a629cfc98128bbd19bdc81d4320654f782f
renanmarx26 <101154828+renanmarx26@users.noreply.github.com>
2024-03-27 15:47:20 -0300

    Material

---
+++ Material.java
@@ -0,0 +1,17 @@
+class Material {
+
+    protected String titulo;
+    protected int anoPublicacao;
+
+    public Material(String titulo, int anoPublicacao) {
+        this.titulo = titulo;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+}
